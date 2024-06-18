6cc2aac7676fe992472ff8e372700786d566e502
hanwinnie <gkswlgp0331@gmail.com>
2024-06-18 09:46:33 +0900

    commit

--- AttendanceSystem/src/main/java/com/exam/Application.java
+++ AttendanceSystem/src/main/java/com/exam/Application.java
@@ -3,11 +3,11 @@ package com.exam;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class AttendanceSystemApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AttendanceSystemApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
-
 }
